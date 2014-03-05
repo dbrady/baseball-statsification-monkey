43036a44c9a7b892517b1e7da91320216848c7bf
David Brady <dbrady@shinybit.com>
2014-03-05 14:27:15 -0700

    Add specs for challenge requirements
    * The stats program now emits answers for all of the questions posed
      in the challenge; no attempt has been made, however, to make them
      even remotely correct
    
    * For now I just want the main bin file to have specs wrapped around
      it asserting that answers for all requirements are given; we'll add
      specs to have them be correct as we roll forward

--- spec/bin/stats_spec.rb
+++ spec/bin/stats_spec.rb
@@ -2,8 +2,36 @@ require 'spec_helper'
 
 describe "stats" do
   describe "when run" do
+    def program_output
+      `bin/stats`
+    end
+
     it "emits 'All done.'" do
-      `bundle exec bin/stats`.should include("All done.")
+      program_output.should include("All done.")
+    end
+
+    it "emits 'Most improved batter 2009->2010:'" do
+      program_output.should include("Most improved batter 2009->2010:")
+    end
+
+    it "emits 'Slugging percentage for all players on the Oakland A's in 2007:'" do
+      program_output.should include("2007 Slugging percentages for Oakland A's:")
+    end
+
+    it "emits AL triple-crown winner for 2011" do
+      program_output.should include("2011 AL Triple Crown Winner:")
+    end
+
+    it "emits NL triple-crown winner for 2011" do
+      program_output.should include("2011 NL Triple Crown Winner:")
+    end
+
+    it "emits AL triple-crown winner for 2012" do
+      program_output.should include("2012 AL Triple Crown Winner:")
+    end
+
+    it "emits NL triple-crown winner for 2012" do
+      program_output.should include("2012 NL Triple Crown Winner:")
     end
   end
 

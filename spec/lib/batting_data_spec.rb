eacf5e2aeaec649c3bdf9c22685d792da2eec4eb
David Brady <dbrady@shinybit.com>
2014-03-15 01:33:26 -0600

    Clean up BattingData
    * Reduce overall complexity signficantly
    
    * integer properties are initted as a loop in initialize as send
      "#{key}=", data[key].to_i
    
    * integer properties are added in a loop as send(key) +
      other.send(key)
    
    * This may be an ill-advised code change; I've made the code tighter
      but less easily accessible to the reader. Adding a stat will be much
      easier now but changing the way stats are dealt with will be much
      harder due to the tightened looping
    
    * This reduces overall flog for this file from 154.7 to 72.1, probably
      good for a grade raise from D to A- or B+ in CodeClimate -- another
      good example of good tools pushing not entirely in a great
      direction, but oh well. This improves the score, but I wish I could
      think of a prettier way to tighten it up

--- spec/lib/batting_data_spec.rb
+++ spec/lib/batting_data_spec.rb
@@ -21,4 +21,26 @@ describe BattingData do
       batting_data.slugging_percentage.should be_within(0.0005).of(0.330)
     end
   end
+
+  describe "#+" do
+    let(:one) { BattingData.new Hash.new(1).merge({player_id: 1}) }
+    let(:two) { BattingData.new Hash.new(2).merge({player_id: 2}) }
+    let(:three) { one + two }
+
+    it "sums the BattingData data" do
+      three.player_id.should == 1
+      three.year.should == 1
+      three.league.should == 1
+      three.games.should == 3
+      three.at_bats.should == 3
+      three.runs.should == 3
+      three.hits.should == 3
+      three.doubles.should == 3
+      three.triples.should == 3
+      three.home_runs.should == 3
+      three.runs_batted_in.should == 3
+      three.stolen_bases.should == 3
+      three.caught_stealing.should == 3
+    end
+  end
 end

cc7a11a630ef7e88a694dd3874c3e7fb537732f9
Benjamin Fleischer <github@benjaminfleischer.com>
2014-05-08 15:10:57 -0500

    Add metric_fu Simplecov formatter configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,17 @@
 # Run `COVERAGE=true bundle exec rspec` to turn on coverage
 if ENV['COVERAGE']
   require 'simplecov'
+  formatters = [SimpleCov::Formatter::HTMLFormatter]
+  begin
+    require 'metric_fu/logger' # workaround mf_log undefined
+    require 'metric_fu/metrics/rcov/simplecov_formatter'
+    formatters << SimpleCov::Formatter::MetricFu
+  rescue LoadError
+    # not running metric_fu formatter
+  end
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    *formatters
+  ]
   SimpleCov.start
 end
 
